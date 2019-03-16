f92f5e36910a20784b173a06ea0ac1d9a14b2bf5
VasiliySk <skobelin@gmail.com>
2019-03-16 20:03:06 +0200

    Добавьте файлы проекта.

---
+++ FishHelper/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FishHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelTargetX = new System.Windows.Forms.Label();
+            this.textBoxCoordX = new System.Windows.Forms.TextBox();
+            this.labelTargetY = new System.Windows.Forms.Label();
+            this.textBoxCoordY = new System.Windows.Forms.TextBox();
+            this.labelTargetCorner = new System.Windows.Forms.Label();
+            this.textBoxCorner = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рыбачим";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelTargetX
+            // 
+            this.labelTargetX.AutoSize = true;
+            this.labelTargetX.Location = new System.Drawing.Point(12, 26);
+            this.labelTargetX.Name = "labelTargetX";
+            this.labelTargetX.Size = new System.Drawing.Size(112, 13);
+            this.labelTargetX.TabIndex = 1;
+            this.labelTargetX.Text = "Адрес Х координаты";
+            // 
+            // textBoxCoordX
+            // 
+            this.textBoxCoordX.Location = new System.Drawing.Point(159, 23);
+            this.textBoxCoordX.Name = "textBoxCoordX";
+            this.textBoxCoordX.Size = new System.Drawing.Size(181, 20);
+            this.textBoxCoordX.TabIndex = 2;
+            // 
+            // labelTargetY
+            // 
+            this.labelTargetY.AutoSize = true;
+            this.labelTargetY.Location = new System.Drawing.Point(12, 73);
+            this.labelTargetY.Name = "labelTargetY";
+            this.labelTargetY.Size = new System.Drawing.Size(112, 13);
+            this.labelTargetY.TabIndex = 3;
+            this.labelTargetY.Text = "Адрес Y координаты";
+            // 
+            // textBoxCoordY
+            // 
+            this.textBoxCoordY.Location = new System.Drawing.Point(159, 70);
+            this.textBoxCoordY.Name = "textBoxCoordY";
+            this.textBoxCoordY.Size = new System.Drawing.Size(181, 20);
+            this.textBoxCoordY.TabIndex = 4;
+            // 
+            // labelTargetCorner
+            // 
+            this.labelTargetCorner.AutoSize = true;
+            this.labelTargetCorner.Location = new System.Drawing.Point(12, 118);
+            this.labelTargetCorner.Name = "labelTargetCorner";
+            this.labelTargetCorner.Size = new System.Drawing.Size(113, 13);
+            this.labelTargetCorner.TabIndex = 5;
+            this.labelTargetCorner.Text = "Адрес угла поворота";
+            // 
+            // textBoxCorner
+            // 
+            this.textBoxCorner.Location = new System.Drawing.Point(159, 115);
+            this.textBoxCorner.Name = "textBoxCorner";
+            this.textBoxCorner.Size = new System.Drawing.Size(181, 20);
+            this.textBoxCorner.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 246);
+            this.Controls.Add(this.textBoxCorner);
+            this.Controls.Add(this.labelTargetCorner);
+            this.Controls.Add(this.textBoxCoordY);
+            this.Controls.Add(this.labelTargetY);
+            this.Controls.Add(this.textBoxCoordX);
+            this.Controls.Add(this.labelTargetX);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Помощник рыболова";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelTargetX;
+        private System.Windows.Forms.TextBox textBoxCoordX;
+        private System.Windows.Forms.Label labelTargetY;
+        private System.Windows.Forms.TextBox textBoxCoordY;
+        private System.Windows.Forms.Label labelTargetCorner;
+        private System.Windows.Forms.TextBox textBoxCorner;
+    }
+}
+
---
+++ FishHelper/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using static FishHelper.EsoWindow;
+
+namespace FishHelper
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var addr = long.Parse(textBoxCoordX.Text, System.Globalization.NumberStyles.HexNumber);
+
+            EsoWindow esoWindow = new EsoWindow();
+            IntPtr hWnd = esoWindow.FindWindow(null, "Elder Scrolls Online"); //Определяем идентификатор процесса
+                        
+            if (hWnd.ToInt32() > 0) //If found
+            {
+                int pid;
+                double targetX = 51.7d;
+                var wHwnd = esoWindow.GetWindowThreadProcessId(hWnd,out pid);
+               // label1.Text = pid.ToString();
+                var processHandle = esoWindow.OpenProcess(0x10, false, pid);
+                var buffer = new byte[8];
+                IntPtr bytesRead;
+                //Активируем окно, прожимаем дважды кноку мыши 
+                esoWindow.SetForegroundWindow(hWnd); //Активируем окно
+                System.Threading.Thread.Sleep(1000);
+                esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_LBUTTONDOWN, new IntPtr(0), new IntPtr(0));
+                esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_LBUTTONUP, new IntPtr(0), new IntPtr(0));
+                System.Threading.Thread.Sleep(1000);
+                esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_LBUTTONDOWN, new IntPtr(0), new IntPtr(0));
+                esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_LBUTTONUP, new IntPtr(0), new IntPtr(0));
+                System.Threading.Thread.Sleep(1000);
+                //Бежим до заданной точки
+                var result = esoWindow.ReadProcessMemory(processHandle, new IntPtr(addr), buffer, (uint)buffer.Length, out bytesRead);
+                while (targetX > BitConverter.ToDouble(buffer, 0))
+                {
+                    esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_KEYDOWN, new IntPtr((ushort)System.Windows.Forms.Keys.W), new IntPtr(0));
+                    System.Threading.Thread.Sleep(100);
+                    result = esoWindow.ReadProcessMemory(processHandle, new IntPtr(addr), buffer, (uint)buffer.Length, out bytesRead);
+                }
+
+                esoWindow.SendMessage(hWnd, (uint)WindowMessages.WM_KEYUP, new IntPtr((ushort)System.Windows.Forms.Keys.W), new IntPtr(0));
+                esoWindow.CloseHandle(processHandle);
+            }
+        }
+           
+    }
+}
